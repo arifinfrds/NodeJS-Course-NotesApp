2ad5a94cabb9d2f7e90628122fdb45b9c436a928
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-30 12:54:06 +0700

    Add `add` command dummy functionality

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const chalk = require('chalk')
 const getNotes = require('./notes.js')
 const yargs = require('yargs')
 
+yargs.command({
+    command: 'add', 
+    description: 'Adding a note',
+    handler: function () {
+        console.log(`Adding a note!`)
+    }
+})
+
 console.log(yargs.argv)
