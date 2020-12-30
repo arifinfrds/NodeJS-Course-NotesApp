7eaff804243893644721f0244803a68da00e7bd0
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-30 12:40:33 +0700

    Installing yargs from npm

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-console.log(process.argv)
+console.log(yargs.argv)
 
 const command = process.argv[2]
 
