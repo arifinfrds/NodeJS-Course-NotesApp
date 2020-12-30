7683b50027dd977669e7372f73fcad05274e3bb0
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-30 12:42:31 +0700

    Delete unused code (since we are going to use command from `yargs`)

--- app.js
+++ app.js
@@ -3,12 +3,3 @@ const getNotes = require('./notes.js')
 const yargs = require('yargs')
 
 console.log(yargs.argv)
-
-const command = process.argv[2]
-
-if (command === `add`) {
-    console.log("Adding note!");
-}
-if (command === `remove`) {
-    console.log("Removing note!");
-}
