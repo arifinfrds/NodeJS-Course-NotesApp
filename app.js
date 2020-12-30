d721a0671b154cfa36d42ad9bfd28f4844b2e4f8
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-12-30 13:04:43 +0700

    Add remove functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const chalk = require('chalk')
 const getNotes = require('./notes.js')
 const yargs = require('yargs')
 
+// MARK: - Add
 yargs.command({
     command: 'add', 
     description: 'Adding a note',
@@ -9,5 +10,16 @@ yargs.command({
         console.log(`Adding a note!`)
     }
 })
+
+// MARK: Remove
+yargs.command({
+    command: `remove`,
+    description: `Remove a note`,
+    handler: function () {
+        console.log(`Removing a note!`);
+    }
+
+})
+
 
 console.log(yargs.argv)
